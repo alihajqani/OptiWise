7e517abe0f68c837b3426dae7e5535060183288b
ali-hjq <ali-hjq@gmail.com>
2025-10-22 19:23:49 +0330

    chore: Update version number to 0.8.0 in main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from PyQt6.QtWidgets import QApplication
 from PyQt6.QtCore import Qt
 from app.main_window import MainWindow
 
-__version__ = "0.7.4"
+__version__ = "0.8.0"
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
